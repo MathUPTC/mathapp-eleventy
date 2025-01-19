c9b3f92c922657ccc880dea226d4ac57780adec8
Fernando Leon <fernandoleonsierra@gmail.com>
2025-01-19 16:47:22 -0500

    rutas

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(eleventyConfig) {
+    
     // Passthrough para copiar archivos estáticos
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
+
+    // Agregar colección de tareas
+    eleventyConfig.addCollection("tareas", function(collectionApi) {
+        const tareasPath = path.join(__dirname, "src", "_data", "tareas.json"); // Cambia esta ruta
+        console.log("Ruta de tareas.json:", tareasPath); // Verifica que sea correcta
+        const tareas = JSON.parse(fs.readFileSync(tareasPath, "utf-8"));
+        return tareas;
+    });
 
     // Configuración de entrada, salida y pathPrefix
     return {
