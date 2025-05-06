0e45d11a5f05ed09ef2cb89bc40b6853e4a591f8
Fernando León <fernandoleonsierra@gmail.com>
2025-05-06 11:40:42 -0500

    eliminación referente

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,28 @@
 const path = require("path");
 
-module.exports = function(eleventyConfig) {
-    
-    // Cargar el archivo JSON de tareas desde la carpeta src/_data
-    const tareasData = require(path.join(__dirname, "src/_data/tareas.json"));
+module.exports = function (eleventyConfig) {
+  // Elimina estas líneas:
+  // const tareasData = require(path.join(__dirname, "src/_data/tareas.json"));
+  // console.log(tareasData);
+  // eleventyConfig.addGlobalData("tareas", tareasData);
 
-    console.log(tareasData); // Verificar los datos cargados
+  // Mantén el resto igual...
+  eleventyConfig.addFilter("split", function (value, delimiter) {
+    return value.split(delimiter);
+  });
 
-    // Agregar datos globales para que estén disponibles en todas las plantillas
-    eleventyConfig.addGlobalData("tareas", tareasData);
+  eleventyConfig.addFilter("jsonify", function (value) {
+    return JSON.stringify(value);
+  });
 
-    eleventyConfig.addFilter("split", function(value, delimiter) {
-        return value.split(delimiter);});
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
 
-    eleventyConfig.addFilter("jsonify", function(value) {
-        return JSON.stringify(value);
-});
-    // Passthrough para copiar archivos estáticos (CSS y JS)
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/js");
-
-    // Configuración de entrada, salida y pathPrefix
-    return {
-        dir: {
-            input: "src",       // Carpeta de entrada
-            output: "docs"      // Carpeta de salida (donde se generarán los archivos estáticos)
-        },
-        pathPrefix: "/mathapp-eleventy/" // Para configurar el path si se usa en un entorno de subcarpeta
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "docs",
+    },
+    pathPrefix: "/mathapp-eleventy/",
+  };
 };
