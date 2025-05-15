bef66e7eca777e4553fc8809b72ef213a308e608
Fernando León <fernandoleonsierra@gmail.com>
2025-05-14 23:27:32 -0500

    configuraciones

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 const path = require("path");
 
 module.exports = function (eleventyConfig) {
-  // Elimina estas líneas:
-  // const tareasData = require(path.join(__dirname, "src/_data/tareas.json"));
-  // console.log(tareasData);
-  // eleventyConfig.addGlobalData("tareas", tareasData);
-
-  // Mantén el resto igual...
   eleventyConfig.addFilter("split", function (value, delimiter) {
     return value.split(delimiter);
   });
